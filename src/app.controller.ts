0ded85fcc3a093dc1f107493210db2507fd4114b
장수빈 <wkdtnqls0506@naver.com>
2024-11-12 16:25:56 +0900

    Feat: movie 로직 service 파일로 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,83 +11,32 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Movie {
-  id: number;
-  title: string;
-}
-
 @Controller('movie')
 export class AppController {
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: '해리포터',
-    },
-    {
-      id: 2,
-      title: '반지의 제왕',
-    },
-  ];
-
-  private idCounter = 3;
-
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies(@Query('title') title: string) {
-    if (!title) {
-      return this.movies;
-    }
-
-    return this.movies.filter((movie) => movie.title.startsWith(title));
+    return this.appService.getManyMovies(title);
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
-
-    if (!movie) {
-      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-    }
-
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
   postMovie(@Body('title') title: string) {
-    const movie: Movie = {
-      id: this.idCounter++,
-      title: title,
-    };
-
-    this.movies.push(movie);
-
-    return movie;
+    return this.appService.createMovie(title);
   }
 
   @Patch(':id')
   patchMovie(@Param('id') id: string, @Body('title') title: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
-
-    if (!movie) {
-      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-    }
-
-    Object.assign(movie, { title });
-
-    return movie;
+    return this.appService.updateMovie(+id, title);
   }
 
   @Delete(':id')
   deleteMovie(@Param('id') id: string) {
-    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-
-    if (movieIndex === -1) {
-      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-    }
-
-    this.movies.splice(movieIndex, 1);
-
-    return id;
+    return this.appService.deleteMovie(+id);
   }
 }
