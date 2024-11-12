4ae1f0af104b980462e225109d4a3b84ac8ec1a0
장수빈 <wkdtnqls0506@naver.com>
2024-11-12 15:59:22 +0900

    Feat: movie 컨트롤러 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,93 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Movie {
+  id: number;
+  title: string;
+}
+
+@Controller('movie')
 export class AppController {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
+  private idCounter = 3;
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies(@Query('title') title: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((movie) => movie.title.startsWith(title));
+  }
+
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
+    }
+
+    return movie;
+  }
+
+  @Post()
+  postMovie(@Body('title') title: string) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title,
+    };
+
+    this.movies.push(movie);
+
+    return movie;
+  }
+
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
+    }
+
+    Object.assign(movie, { title });
+
+    return movie;
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
+
+    if (movieIndex === -1) {
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
+    }
+
+    this.movies.splice(movieIndex, 1);
+
+    return id;
   }
 }
