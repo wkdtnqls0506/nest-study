6c9f797102ff670f0a52bccde50d3e07c90523ea
장수빈 <wkdtnqls0506@naver.com>
2024-11-13 19:45:22 +0900

    Feat: class validator 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 정의하지 않은 값들은 전달되지 않도록 설정 (default: false)
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
