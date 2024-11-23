a7ead087a977299d7a96b1b6da1e3372248070af
장수빈 <wkdtnqls0506@naver.com>
2024-11-23 23:52:07 +0900

    Feat: Auth Guard가 필요없는 부분에 커스텀 @Public 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,17 +9,20 @@ import {
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './strategy/local.strategy';
 import { JwtAuthGuard } from './strategy/jwt.strategy';
+import { Public } from './decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('register')
   // Authorization: Basic $token
   registerUser(@Headers('authorization') token: string) {
     return this.authService.register(token);
   }
 
+  @Public()
   @Post('login')
   // Authorization: Basic $token
   loginUser(@Headers('authorization') token: string) {
--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -1,9 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Public } from '../decorator/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
-    // 1. 요청에서 user 객체가 존재하는지 확인 -> 존재하는 것 자체가 인증된 것(Middleware에서 전부 검증 완료된 것)
+    // 만약 public decotartion이라면, 모든 로직을 bypass
+    const isPublic = this.reflector.get(Public, context.getHandler()); // @Public() decorator가 붙어있으면 객체를 반환하고, 붙어 있지 않다면 undefined를 반환
+
+    if (isPublic) return true;
+
+    // 요청에서 user 객체가 존재하는지 확인 -> 존재하는 것 자체가 인증된 것(Middleware에서 전부 검증 완료된 것)
     const request = context.switchToHttp().getRequest();
 
     if (!request.user || request.user.type !== 'access') {
